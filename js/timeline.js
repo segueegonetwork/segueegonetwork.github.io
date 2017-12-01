5bea1dca90c21baabb000efb6f5bf47745ec2a41
Terrance Law <pmlaw@Terrances-MacBook-Pro.local>
2017-12-01 05:46:31 -0500

    timeline modification

--- js/timeline.js
+++ js/timeline.js
@@ -24,19 +24,6 @@ var Timeline = {
 			.attr("y2", 55)
 			.style("stroke", "#d3d3d3");
 
-		// draw circles
-		self.svg.selectAll("cirlce")
-			.data(Database.dateStringArray)
-			.enter()
-			.append("circle")
-			.attr("cx", function(d, i) {
-				return xScale(i)
-			})
-			.attr("cy", 55)
-			.attr("r", 5)
-			.style("fill", "white")
-			.style("stroke", "#d3d3d3");
-
 		// create text
 		var dateGroup = self.svg.selectAll(".date")
 			.data(Database.dateStringArray)
@@ -47,7 +34,7 @@ var Timeline = {
 				return "translate(" + xScale(i) + ", 40)" + " rotate(-45)";
 			});
 
-		dateGroup.each(function(d) {
+		dateGroup.each(function(d, i) {
 			var parseDate = d3.time.format("%Y-%m").parse;
 			var formatTime = d3.time.format("%b %y");
 			var dateString = formatTime(parseDate(d));
@@ -64,42 +51,65 @@ var Timeline = {
 				.attr("ry", 3)
 				.attr("width", bbox.width + 6)
 				.attr("height", bbox.height + 4);
+
+			d3.select(this).append("circle")
+				.attr("cx", xScale(i))
+				.attr("cy", 55)
+				.attr("r", 5)
+				.attr("transform", "rotate(45) " + " translate(" + -xScale(i) + ", -40)");
 		});
 	},
 	highlight: function(timeIndex) {
 		var self = this;
+		var targetDateGroup = self.svg.selectAll(".date")[0][timeIndex];
 
 		// restore all
 		self.svg.selectAll("text")
 			.style("font-size", null)
 			.style("font-weight", null);
 		self.svg.selectAll("circle")
-			.style("fill", "white");
+			.style("fill", null);
 
 		// text
-		var targetText = self.svg.selectAll("text")[0][timeIndex];
-		d3.select(targetText)
+		var text = d3.select(targetDateGroup).select("text")
 			.style("font-size", 13)
 			.style("font-weight", "bold");
 
+		// rect
+		var bbox = text.node().getBBox();
+		d3.select(targetDateGroup).select("rect")
+			.style("x", bbox.x - 3)
+			.style("y", bbox.y - 2)
+			.style("width", bbox.width + 6)
+			.style("height", bbox.height + 4);
+
 		// circle
-		var targetCircle = self.svg.selectAll("circle")[0][timeIndex];
-		d3.select(targetCircle)
+		d3.select(targetDateGroup).select("circle")
 			.style("fill", "#d3d3d3");
 	},
 	removeHighlight: function() {
 		var self = this;
 
-		self.svg.selectAll("text")
-			.style("font-size", null)
-			.style("font-weight", null);
-		self.svg.selectAll("circle")
-			.style("fill", "white");
+		self.svg.selectAll(".date").each(function() {
+			var text = d3.select(this).select("text")
+				.style("font-size", null)
+				.style("font-weight", null);
+
+			var bbox = text.node().getBBox();
+			d3.select(this).select("rect")
+				.style("x", bbox.x - 3)
+				.style("y", bbox.y - 2)
+				.style("width", bbox.width + 6)
+				.style("height", bbox.height + 4);
+
+			d3.select(this).select("circle")
+				.style("fill", null);
+		});
 	},
 	select: function(timeIndex) {
 		var self = this;
 		var targetDateGroup = self.svg.selectAll(".date")[0][timeIndex];
-		
+
 		d3.select(targetDateGroup)
 			.classed("selected", true);
 	}	
